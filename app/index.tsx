e3f3da2af8afba06eefba42d971e00341fc25103
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-11 02:38:19 +0200

    chore: Update tailwind CSS configuration and fix code formatting

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react'
+import { Text, View } from 'react-native'
 
-type Props = {};
-
-const index = (props: Props) => {
+const index = () => {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="flex-1 items-center justify-center">
       <Text>other</Text>
     </View>
-  );
-};
+  )
+}
 
-export default index;
+export default index
