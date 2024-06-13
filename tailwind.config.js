91de947e23dcd973230a2f4e2eb709f9aac52a6f
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-14 02:53:40 +0300

    chore: update tailwind CSS configuration and add new color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = {
+  danger: {},
+  warning: {},
+  success: {},
+  light: {
+    50: '#FFFFFF',
+    100: '#F5F0F0',
+    200: '#E5DBDB',
+    800: '#8A6161',
+    900: '#876363',
+    950: '#171212',
+  },
+  dark: {
+    50: '#171212',
+    100: '#382929',
+    200: '#261C1C',
+    800: 'BA9C9C',
+    900: '#B89E9E',
+    950: '#FFFFFF',
+  },
+}
+
 module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
 }
