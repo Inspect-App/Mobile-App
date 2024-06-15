d62cf27ec05258a79ec2eed7437461f94f6fd8ff
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-15 03:51:37 +0300

    refactor: optimize video list rendering and add swipe-to-delete functionality

--- app/(tabs)/viewPendingVideos/index.tsx
+++ app/(tabs)/viewPendingVideos/index.tsx
@@ -1,8 +1,84 @@
+import { AntDesign } from '@expo/vector-icons'
+import { Link } from 'expo-router'
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
+import TinderCard from 'react-tinder-card'
+import { useColorScheme } from '@/hooks/useColorScheme'
 
-const index = () => {
-  return <Text>index</Text>
+function Simple() {
+  const videos = [
+    {
+      title: 'Video 1',
+      duration: '1:00',
+    },
+    {
+      title: 'Video 2',
+      duration: '2:00',
+    },
+    {
+      title: 'Video 3',
+      duration: '3:00',
+    },
+    {
+      title: 'Video 4',
+      duration: '4:00',
+    },
+    {
+      title: 'Video 5',
+      duration: '5:00',
+    },
+  ]
+
+  const swiped = (direction: string, nameToDelete: string) => {
+    console.log('removing: ' + nameToDelete)
+  }
+
+  const outOfFrame = (name: string) => {
+    console.log(name + ' left the screen!')
+  }
+
+  const colorScheme = useColorScheme()
+
+  return (
+    <View className="flex h-screen w-screen items-center bg-light-50 px-4 py-4 dark:bg-dark-50">
+      <View className="h-[500px] w-full">
+        {videos.map((video) => {
+          return (
+            <>
+              <TinderCard
+                key={video.title}
+                onSwipe={(dir) => {
+                  swiped(dir, video.title)
+                }}
+                onCardLeftScreen={() => {
+                  outOfFrame(video.title)
+                }}
+              >
+                <View className="absolute left-4 h-[550px] w-[320px] resize rounded-2xl bg-light-100 dark:bg-dark-100">
+                  <View className="absolute bottom-0 w-full px-6 py-2">
+                    <Text className="text-2xl font-bold text-light-950 dark:text-dark-950">
+                      {video.title}
+                    </Text>
+                    <Text className="text-lg text-light-800 dark:text-dark-900">
+                      {video.duration}
+                    </Text>
+                  </View>
+                </View>
+              </TinderCard>
+            </>
+          )
+        })}
+      </View>
+      <View className="mt-20 flex w-full items-center justify-center">
+        <Link href="/inspect">
+          <View className="flex flex-row justify-center gap-2 rounded-full bg-light-100 px-5 py-4 dark:bg-dark-100">
+            <AntDesign name="camera" size={24} color={colorScheme === 'dark' ? 'white' : 'black'} />
+            <Text className="text-xl font-bold text-light-950 dark:text-dark-950">Inspect</Text>
+          </View>
+        </Link>
+      </View>
+    </View>
+  )
 }
 
-export default index
+export default Simple
